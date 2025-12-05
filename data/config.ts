823f9324925c0b5cce9098ed672aefc59ee274f3
torrgacor <banyuasin425@gmail.com>
2025-12-05 20:16:46 +0700

    Update config.ts

--- data/config.ts
+++ data/config.ts
@@ -9,8 +9,8 @@ export const pterodactylConfig = {
   },
   // Public Server Configuration
   public: {
-    domain: "https://tokopanel157-private216.mts4you.biz.id", // ubah sesuai domain public panel
-    apiKey: "ptla_ms1X6SBb1SZQcSJrtX4xx16O9FwZHWBXbpsEPknrke1", // ubah sesuai public API key
+    domain: "https://tokopanel157-public209.mts4you.biz.id", // ubah sesuai domain public panel
+    apiKey: "ptla_ueVeAgspM7fITWiHFy6KLkM44w2M1utAxK5xyDY7EQZ", // ubah sesuai public API key
     nests: "5", 
     egg: "15", 
     location: "1", // location panel 
